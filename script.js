50a76a18b48fbd98bf04c3e560c58f4ee0fad9c0
Josue Gonzalez <josuegr834@gmail.com>
2024-06-04 18:44:41 -0300

    Moviendo el proyecto a un repo individual

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const dropdownBtn = document.getElementById('dropdown-btn');
+    const navbar = document.querySelector('.navbar');
+
+    dropdownBtn.addEventListener('click', function () {
+        navbar.classList.toggle('show');
+    });
+});
